a9fc8b9b9054715ae1f4322b144c6be3c15a4679
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-11-04 13:18:42 +0530

    learning tailwind in the react project'

---
+++ tailwindProject/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  
+  return (
+
+    //<div style={{backgroundColor:blue}}></div> this is the raw css code
+    <>
+      <div className='flex justify-center bg-blue-100'>
+        <div>
+          child1
+        </div>
+        <div>
+          child2
+        </div>
+        <div>
+          child3
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ tailwindProject/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//content represents where the frontend code or where did we write the frontend code give the path
