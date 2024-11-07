385ca0781e233b2be84f88db798a062d0b0da8b2
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2024-11-07 10:37:55 +0530

    the code changed for the grid design

--- tailwindProject/src/App.jsx
+++ tailwindProject/src/App.jsx
@@ -3,17 +3,15 @@ import './App.css'
 function App() {
   
   return (
-
-    //<div style={{backgroundColor:blue}}></div> this is the raw css code
     <>
-      <div className='flex justify-center bg-blue-100'>
-        <div>
+      <div className='grid grid-cols-12'>
+        <div className='bg-blue-500 col-span-4'>
           child1
         </div>
-        <div>
+        <div className='bg-red-400 col-span-6'>
           child2
         </div>
-        <div>
+        <div className='bg-violet-500 col-span-2'>
           child3
         </div>
       </div>
